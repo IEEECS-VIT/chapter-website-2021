2c45aa4769a8108465b5791a08b7b14a4ab9a2a1
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-02-25 21:48:04 +0530

    Navbar Scroll added

--- src/components/sidebar/navbar.js
+++ src/components/sidebar/navbar.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React,{useState,useRef,useEffect} from "react"
 import { useThemeValue } from "../../utils/context"
+import useWindowScrollPosition from "@rehooks/window-scroll-position";
 
 import logo from "../../images/logo.png"
 import darklogo from "../../images/darklogo.png"
@@ -9,13 +10,14 @@ import styles from './sidebar.module.css'
 
 const Menu = ({ open ,setOpen}) => {
   const { toggleTheme, isDark } = useThemeValue()
+ return (
  
+ 
+  <div >
   
-  return (
-  <div className={styles.navbar}>
-  <img src={isDark? darklogo:logo} />
   <nav className={`${styles.StyledMenu} ${isDark? styles.darkmode:styles.lightmode}`} 
   style={{transform: !(open) ? "translateX(100%)": "translateX(0)"}}>
+  
       <a href="#Home" onClick={() => setOpen(!open)}>Home</a>
       <a href="#About" onClick={() => setOpen(!open)}>About</a>
       <a href="#Events" onClick={() => setOpen(!open)} >Events</a>
@@ -28,6 +30,7 @@ const Menu = ({ open ,setOpen}) => {
 
     </nav>
     </div>
+    
   )
 }
 
@@ -35,8 +38,12 @@ const Menu = ({ open ,setOpen}) => {
 
 
 const Burger = ({ open, setOpen }) => {
-  const { toggleTheme, isDark } = useThemeValue()
+  const { isDark } = useThemeValue()
+  
+ 
   return (
+ 
+  
     <button 
     onClick={() => setOpen(!open)} 
     className={styles.styledBurger} > 
@@ -46,23 +53,45 @@ const Burger = ({ open, setOpen }) => {
       <div className={`${(open) ? styles.open: styles.closed} ${isDark? styles.darkmode:styles.lightmode}`}/>
       
     </button>
+  
   )
 }
 
 
 const Header = () => {
-   const { toggleTheme, isDark } = useThemeValue()
+   const { isDark } = useThemeValue()
   const [open, setOpen] = React.useState(false);
   const node = React.useRef();
+
+  const [change, setChange] = useState(false);
+  const changePosition = 80;
+
+  let position = useWindowScrollPosition();
+
+  if (position.y > changePosition && !change) {
+    setChange(true);
+  }
+
+  if (position.y <= changePosition && change) {
+    setChange(false);
+  }
+ 
   return (
-  
-      <div className={isDark? styles.dark:null}>
-      <div ref={node}>
+      <>
+
       
+      
+      
+      <div ref={node} className={`${styles.showNav} ${isDark? styles.dark:null}`}
+      style={{backgroundColor: change? "#393e46":"transparent" , transitionTimingFunction: "ease-in"}}>
+      <img className={styles.logo}src={isDark? darklogo:logo} />
         <Burger open={open} setOpen={setOpen} />
+      
         <Menu open={open} setOpen={setOpen} />
-      </div>
-    </div>
+        </div>
+      
+    
+    </>
   )  
 }
 
--- src/sections/landing/index.js
+++ src/sections/landing/index.js
@@ -3,9 +3,11 @@ import styles from "./styles.module.css"
 import { useThemeValue } from "../../utils/context"
 import LightArrow from "../../images/Down_light.png"
 import DarkArrow from "../../images/Down_dark.png"
+import Darkbg from "../../images/darkbg.png"
+import Lightbg from "../../images/Lightbg.png"
 
 const Landing = () => {
-  const { toggleTheme, isDark } = useThemeValue()
+  const { isDark } = useThemeValue()
   const color = isDark === true ?  "white" :"black";
   const backgroundColor = isDark === true ?  "black":"white";
   console.log(isDark)
@@ -17,6 +19,7 @@ const Landing = () => {
   return (
     
     <div id="Home" className={styles.home}>
+    
   
     <div className={styles.heading}>
      <p > WHAT ARE WE </p>
@@ -27,7 +30,7 @@ const Landing = () => {
      <p className={styles.text} >We strive to be the leading provider of technical information,
      community services, and personalized support to the world’s
     computer science and technology communities </p>
-    <a href="#About" className={styles.btn}><img src={isDark? LightArrow:DarkArrow}/></a>
+    <a href="#About" className={styles.btn}><img className={styles.arrow} src={isDark? LightArrow:DarkArrow}/></a>
     </div>
     
   )
