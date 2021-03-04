4a2319eeb429e457af748071f97ad51fe8b4b8ae
Shambhavi <shambhavis20@gmail.com>
2021-03-04 14:27:52 +0530

    closes #2

--- src/sections/announcement/index.js
+++ src/sections/announcement/index.js
@@ -1,12 +1,27 @@
 import React from "react"
 import styles from "./announcementstyle.module.css"
-const Announcement = () => {
+import close from '../../images/close.png'
+class Announcement extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      isClosed:false
+    }
+    this.handleClick=this.handleClick.bind(this)
+  }
+  handleClick(){
+    this.setState({isClosed:true})
+  }
+  render(){
+   
   return (
-    <div className={styles.achievement}>
-      <div className={styles.text}>Announcement: Lorem ipsum dolor sit amet,</div>
-      
+    <div className={this.state.isClosed?styles.hideclass:styles.announcement}>
+      <div className={styles.text}>Announcement: Lorem ipsum dolor sit amet,
+      <img src={close} className={styles.close} onClick={this.handleClick}></img>
+      </div>
     </div>
   )
+}
 }
 
 export default Announcement
