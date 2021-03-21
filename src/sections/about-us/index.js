e11abc253a9e3927834c687434279639ebf2d9ba
Shambhavi <shambhavis20@gmail.com>
2021-03-21 17:55:48 +0530

    ellipses done

--- src/sections/about-us/index.js
+++ src/sections/about-us/index.js
@@ -9,7 +9,7 @@ const AboutUs = () => {
                 <img className={styles.dot} src={dot} alt="dotted"></img>
                 <div className={styles.aboutus}>About Us</div>
                 <img className={styles.clubimg} src={clubimg} alt="club"></img>
-                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu at mattis. Cras a erat at lacus aliquam sodales. Etiam id metus arcu. Fusce porttitor pharetra turpis sit amet lobortis. Quisque aliquet purus nec purus dignissim sodales. Phasellus ac auctor magna, eget feugiat ipsum..</div>
+                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu at mattis. Cras a erat at lacus aliquam sodales. Etiam id metus arcu. Fusce porttitor pharetra turpis sit amet lobortis. Quisque aliquet purus nec purus dignissim sodales. Phasellus ac auctor magna, eget feugiat ipsum.consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu</div>
                 <div className={styles.circle1}></div>
                 <div className={styles.circle2}></div>
                 <div className={styles.circle3}></div>
