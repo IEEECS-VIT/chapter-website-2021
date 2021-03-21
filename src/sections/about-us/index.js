fd4635a8ce2d00b58e8be0b3fe2c55eb5e101aad
Shambhavi <shambhavis20@gmail.com>
2021-03-22 00:00:11 +0530

    added dark mode for about us

--- src/sections/about-us/index.js
+++ src/sections/about-us/index.js
@@ -2,14 +2,16 @@ import React from "react"
 import styles from './aboutusstyle.module.css';
 import clubimg from '../../images/club.png'
 import dot from '../../images/Bg_Lisght.png'
+import { useThemeValue } from "../../utils/context"
 const AboutUs = () => {
+  const { isDark } = useThemeValue()
   return (
     <div className={styles.about}>
                 <div className={styles.hollowcircle}></div>
                 <img className={styles.dot} src={dot} alt="dotted"></img>
-                <div className={styles.aboutus}>About Us</div>
+                <div className={`${styles.aboutus} ${isDark && styles.dark}`}>About Us</div>
                 <img className={styles.clubimg} src={clubimg} alt="club"></img>
-                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu at mattis. Cras a erat at lacus aliquam sodales. Etiam id metus arcu. Fusce porttitor pharetra turpis sit amet lobortis. Quisque aliquet purus nec purus dignissim sodales. Phasellus ac auctor magna, eget feugiat ipsum.consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu</div>
+                <div className={`${styles.text} ${isDark && styles.dark}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu at mattis. Cras a erat at lacus aliquam sodales. Etiam id metus arcu. Fusce porttitor pharetra turpis sit amet lobortis. Quisque aliquet purus nec purus dignissim sodales. Phasellus ac auctor magna, eget feugiat ipsum.consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu</div>
                 <div className={styles.circle1}></div>
                 <div className={styles.circle2}></div>
                 <div className={styles.circle3}></div>
