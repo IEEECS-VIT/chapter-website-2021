f914e39b19d8c50b8e76fa6b2ca198bf3825c6bb
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2021-03-24 22:13:13 +0530

    🔧 updated footer

--- src/components/template.js
+++ src/components/template.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import React from "react"
 import ThemeProvider from "../utils/theme"
 import Navbar from "./sidebar/navbar"
-=======
-import React, { useContext } from "react"
-=======
-import React from "react"
->>>>>>> 🔧 fixed some errors
-
-import ThemeProvider from "../utils/theme"
-import Navbar from "./navbar"
 
 import Footer from "./Footer"
->>>>>>> closes #3
 
 const Template = ({ children }) => {
   return (
@@ -21,18 +11,8 @@ const Template = ({ children }) => {
       <div>
         <Navbar />
         {children}
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <h1>Footer</h1>
-      </div>
-=======
         <Footer/>
         </div>
->>>>>>> closes #3
-=======
-        <Footer />
-      </div>
->>>>>>> 🔧 fixed some errors
     </ThemeProvider>
   )
 }
