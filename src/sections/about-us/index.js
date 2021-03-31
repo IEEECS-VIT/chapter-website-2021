1cf3e069b997379e9bd7621d12bc5f11d87b69db
Shambhavi <shambhavis20@gmail.com>
2021-03-31 11:42:38 +0530

    dark bg removed from text

--- src/sections/about-us/index.js
+++ src/sections/about-us/index.js
@@ -9,9 +9,9 @@ const AboutUs = () => {
     <div className={`${styles.about} ${isDark && styles.dark}`}>
                 <div className={styles.hollowcircle}></div>
                 <img className={styles.dot} src={dot} alt="dotted"></img>
-                <div className={`${styles.aboutus} ${isDark && styles.dark}`}>About Us</div>
+                <div className={styles.aboutus}>About Us</div>
                 <img className={styles.clubimg} src={clubimg} alt="club"></img>
-                <div className={`${styles.text} ${isDark && styles.dark}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu at mattis. Cras a erat at lacus aliquam sodales. Etiam id metus arcu. Fusce porttitor pharetra turpis sit amet lobortis. Quisque aliquet purus nec purus dignissim sodales. Phasellus ac auctor magna, eget feugiat ipsum.consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu</div>
+                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu at mattis. Cras a erat at lacus aliquam sodales. Etiam id metus arcu. Fusce porttitor pharetra turpis sit amet lobortis. Quisque aliquet purus nec purus dignissim sodales. Phasellus ac auctor magna, eget feugiat ipsum.consectetur adipiscing elit. In nec lacinia velit. Phasellus tempor in est sed fringilla. Etiam nec sem elit. Nunc rhoncus hendrerit arcu</div>
                 <div className={styles.circle1}></div>
                 <div className={styles.circle2}></div>
                 <div className={styles.circle3}></div>
