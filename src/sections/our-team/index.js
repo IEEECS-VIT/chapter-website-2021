0b9cb2210063e3786aad7ee8aa291990f0760f22
lakhan_nad <lakhannad1999@gmail.com>
2021-04-15 00:28:33 +0530

    images added for team

--- src/sections/our-team/index.js
+++ src/sections/our-team/index.js
@@ -8,32 +8,114 @@ const OurTeamSection = () => {
   const { isDark } = useThemeValue()
 
   const cardInfo1 = {
-    name: "Doggo Pupper",
-    image: require("../../images/pupper.jpg"),
-    position: "Doggo CEO",
-    linkedIn: "https://linkedin.com/",
-    twitter: "https://twitter.com/",
-    personal: "https://dog.ceo/",
+    name: "Aayushi Tated",
+    image: require("./images/aayushi.jpg"),
+    position: "Vice Chair",
+    linkedIn: "https://www.linkedin.com/in/aayushi-tated-898805193/",
+    github: "https://github.com/aayushi2000",
+    personal: "https://auth.geeksforgeeks.org/user/aayushitated2000/articles"
   }
 
   const cardInfo2 = {
-    name: "Doggo Pupper",
-    image: require("../../images/pupper.jpg"),
-    position: "Doggo CEO",
-    instagram: "https://instagram.com/",
-    facebook: "https://facebook.com/",
-    personal: "https://dog.ceo/",
+    name: "Mohit Hotwani",
+    image: require("./images/mohit.jpg"),
+    position: "Chair",
+    linkedIn: "https://www.linkedin.com/in/mohit-hotwani-719492167/",
+	  github: "https://github.com/mohithotwani01",
   }
 
   const cardInfo3 = {
-    name: "Doggo Pupper",
-    image: require("../../images/pupper.jpg"),
-    position: "Doggo CEO",
-    linkedIn: "https://linkedin.com/",
-    twitter: "https://twitter.com/",
-    personal: "https://dog.ceo/",
+    name: "Prerna Sultania",
+    image: require("./images/aayushi.jpg"),
+    position: "HR & Operations Head",
+    linkedIn: "https://www.linkedin.com/in/prerna-sultania-762b79177/",
+    github: "https://github.com/PrernaSultania"
   }
 
+  const cardInfo4 = {
+    name: "Lakhan Nad",
+    image: require("./images/lakhan.jpg"),
+    position: "Technical Head",
+    linkedIn: "https://www.linkedin.com/lakhan-nad",
+    github:"https://github.com/Lakhan-Nad",
+  }
+
+  const cardInfo5 = {
+    name: "Aditya Menon",
+    image: require("./images/aditya.jpg"),
+    position: "Projects Head",
+    personal: "https://www.supraditya.com/",
+    linkedIn: "https://www.linkedin.com/in/supraditya/",
+    github: "https://github.com/supraditya"
+  }
+
+  const cardInfo6 = {
+    name: "Nikita Anand",
+    image: require("./images/nikita.jpg"),
+    position: "Design Head",
+    linkedIn: "https://www.linkedin.com/in/nikita-anand-381608195/",
+    github:"https://github.com/nikitaanand16",
+  }
+
+  const cardInfo7 = {
+    name: "Arpit Bhattar",
+    image: require("./images/arpit.png"),
+    position: "Finance Head",
+    linkedIn: "https://www.linkedin.com/in/arpit-bhattar-6b5a04184",
+    github: "https://github.com/apt-bhattar"
+  }
+
+  const cardInfo8 = {
+    name: "Kaustubh Singh",
+    image: require("./images/kaustubh.jpg"),
+    position: "Public Relations Head",
+    personal: "http://www.kaustubh.live/",
+    linkedIn: "https://www.linkedin.com/in/kaustubh72/",
+    github: "https://github.com/Kaustubh72"
+  }
+
+  const cardInfo9 = {
+    name: "Ayush Roy",
+    image: require("./images/ayush.jpeg"),
+    position: "Events Head",
+    linkedIn: "https://www.linkedin.com/in/ayush-roy-40838b191/",
+    github: "https://github.com/ayushroy64"
+  }
+
+  const cardInfo10 = {
+    name: "Ipsita Goel",
+    image: require("./images/aayushi.jpg"),
+    position: "Marketing & Outreach Head",
+    linkedIn: "https://www.linkedin.com/in/ipsitagoel/",
+    github:"https://github.com/ipsitagoel",
+  }
+
+  const cardInfo11 = {
+    name: "Shaolin Kataria",
+    image: require("./images/shaolin.jpg"),
+    position: "Research Head",
+    linkedIn: "https://www.linkedin.com/in/shaolink/",
+    github: "https://github.com/shaolink8",
+    youtube: "https://www.youtube.com/user/ShaolinKataria",
+  }
+
+  const cardInfo12 = {
+    name: "Kamlendra Pratap Singh",
+    image: require("./images/kamlendra.jpg"),
+    position: "Strategic Head",
+    linkedIn: "https://www.linkedin.com/in/kamlendra-pratap-singh-372427192/",
+    github: "https://github.com/kps1310"
+  }
+
+  const cardInfo13 = {
+    name: "Sunjol Singh",
+    image: require("./images/sunjol.jpg"),
+    position: "Technical Lead",
+    linkedIn: "https://www.linkedin.com/in/sunjol-singh-paul",
+    github: "https://github.com/sunjol",
+  }
+
+ 
   return (
     <div className={`${classes.container} ${isDark && classes.dark}`}>
       <div className={classes.headerWrapper}>
@@ -56,16 +138,16 @@ const OurTeamSection = () => {
         <TeamCard cardInfo={cardInfo1} />
         <TeamCard cardInfo={cardInfo2} />
         <TeamCard cardInfo={cardInfo3} />
-        <TeamCard cardInfo={cardInfo1} />
-        <TeamCard cardInfo={cardInfo2} />
-        <TeamCard cardInfo={cardInfo3} />
-        <TeamCard cardInfo={cardInfo1} />
-        <TeamCard cardInfo={cardInfo2} />
-        <TeamCard cardInfo={cardInfo3} />
-        <TeamCard cardInfo={cardInfo1} />
-        <TeamCard cardInfo={cardInfo2} />
-        <TeamCard cardInfo={cardInfo3} />
-        <TeamCard cardInfo={cardInfo3} />
+        <TeamCard cardInfo={cardInfo4} />
+        <TeamCard cardInfo={cardInfo5} />
+        <TeamCard cardInfo={cardInfo6} />
+        <TeamCard cardInfo={cardInfo7} />
+        <TeamCard cardInfo={cardInfo8} />
+        <TeamCard cardInfo={cardInfo9} />
+        <TeamCard cardInfo={cardInfo10} />
+        <TeamCard cardInfo={cardInfo11} />
+        <TeamCard cardInfo={cardInfo12} />
+        <TeamCard cardInfo={cardInfo13} />
       </div>
     </div>
   )
