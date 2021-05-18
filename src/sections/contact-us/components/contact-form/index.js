b0d4443c574692b56420b0bd46ea8777fae61adb
Lakhan Nad <lakhannad1999@gmail.com>
2021-05-18 13:58:27 +0530

    fix: axios changed to fetch api and <super> tag changed to <strong> in eventsMain

--- src/sections/contact-us/components/contact-form/index.js
+++ src/sections/contact-us/components/contact-form/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import classes from "./styles.module.css"
-import axios from "axios"
 import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 
@@ -9,24 +8,13 @@ const ContactForm = () => {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [message, setMessage] = useState("")
-  const validateContent = () => {
-    if (!/^[\w.-]+@[\w.-]+\.\w+$/.test(email)) {
-      // invalid email
-    }
-    if (name.length < 2) {
-      // invalid name
-    }
-    if (message.length < 3) {
-      // invalid message
-    }
-  }
   const notify = status => {
     if (status === "success") {
       // Success toast
       toast.success("Form Submitted!", {
         position: "bottom-center",
         autoClose: 3000,
-        hideProgressBar: false,
+        hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: true,
@@ -34,10 +22,10 @@ const ContactForm = () => {
       })
     } else if (status === "validation") {
       // Validation error toast
-      toast.warn("Some Form Fields are Incorrectly Filled ", {
+      toast.warning("Some Form Fields are Incorrectly Filled ", {
         position: "bottom-center",
         autoClose: 3000,
-        hideProgressBar: false,
+        hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: true,
@@ -46,37 +34,48 @@ const ContactForm = () => {
     } else if (status === "error") {
       // Other Error toast
       toast.error(
-        "An Error Occurred During Submission. Please Try Again Later ",
+        "An Error Occurred During Submission. Please Try Again Later",
         {
           position: "bottom-center",
           autoClose: 3000,
-          hideProgressBar: false,
+          hideProgressBar: true,
           closeOnClick: true,
           pauseOnHover: false,
           draggable: true,
           progress: undefined,
         }
       )
+    } else if (status === "network-error") {
+      // network error toast
+      toast.error("Unable to Send Request", {
+        position: "bottom-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      })
     }
   }
   const handleSubmit = event => {
     event.preventDefault()
-    validateContent()
     setSubmitting(true)
-    axios
-      .post(
-        "/.netlify/functions/send-mail",
-        {
-          name,
-          email,
-          message,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      )
+    fetch("/.netlify/functions/send-mail", {
+      body: JSON.stringify({
+        name,
+        email,
+        message,
+      }),
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      method: "POST",
+      referrerPolicy: "no-referrer",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then(res => {
         if (res.status === 200) {
           // success
@@ -85,21 +84,23 @@ const ContactForm = () => {
           setMessage("")
           notify("success")
         } else if (res.status === 403) {
-          // validation error
           notify("validation")
         } else {
-          // some other error
           notify("error")
         }
         setSubmitting(false)
       })
       .catch(err => {
-        // some error
+        notify("network-error")
         setSubmitting(false)
       })
   }
   return (
-    <form className={classes.formContainer} onSubmit={handleSubmit}>
+    <form
+      className={classes.formContainer}
+      onSubmit={handleSubmit}
+      noValidate={false}
+    >
       <div className={classes.formgroup}>
         <input
           id="contact_name"
@@ -111,6 +112,7 @@ const ContactForm = () => {
           }}
           required
           placeholder="Your Name"
+          minLength={5}
         />
       </div>
       <div className={classes.formgroup}>
@@ -140,7 +142,6 @@ const ContactForm = () => {
         />
       </div>
       <div className={classes.formgroup}>
-        {/* Putting onclick={notify("success")} will display the toast as intended, but form submission configuration not working */}
         <button
           className={classes.submitButton}
           type="submit"
